7e40cdd7da4a76ade8861e5753d823cd7b24548e
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-01-25 14:25:31 +0530

    refactor: update model IDs from UUID to int and enhance error messages in repository methods

--- api/database/models.py
+++ api/database/models.py
@@ -1,11 +1,10 @@
-from sqlmodel import Field, SQLModel, Relationship, LargeBinary, UUID
+from sqlmodel import Field, SQLModel, Relationship, LargeBinary
 from datetime import date as Date
 from typing import List
-from uuid import uuid4
 
 
 class BaseModel(SQLModel):
-    id: UUID = Field(primary_key=True, default_factory=uuid4, default=lambda: uuid4())
+    id: int | None = Field(primary_key=True)
 
 
 class User(BaseModel, table=True):
@@ -35,10 +34,10 @@ class Categories(BaseModel, table=True):
 class Listing(BaseModel, table=True):
     listing_name: str
     listing_price: float
-    category_id: UUID = Field(foreign_key="categories.id")
+    category_id: int | None = Field(foreign_key="categories.id")
     desc: str
 
-    user_id: UUID = Field(foreign_key="user.id")
+    user_id: int | None = Field(foreign_key="user.id")
     user: User = Relationship(back_populates="listings")
 
     medias: List["Media"] = Relationship(
@@ -73,14 +72,14 @@ class Fields(BaseModel, table=True):
     field_name: str
     field_value: str
 
-    listing_id: UUID = Field(foreign_key="listing.id")
+    listing_id: int | None = Field(foreign_key="listing.id")
     listing: Listing = Relationship(back_populates="fields")
 
 
 class Media(BaseModel, table=True):
     data: bytes = Field(sa_column_args=[LargeBinary])
 
-    listing_id: UUID = Field(foreign_key="listing.id")
+    listing_id: int | None = Field(foreign_key="listing.id")
     listing: Listing = Relationship(
         back_populates="medias",
         sa_relationship_kwargs={
